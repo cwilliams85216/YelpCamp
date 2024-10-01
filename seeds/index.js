63ab4c2369a1428a822771b9476774eb0de873e2
Chelsey W. <55863583+cwilliams85216@users.noreply.github.com>
2024-10-01 10:58:42 -0500

    Update index.js
    attempt for seeding

--- seeds/index.js
+++ seeds/index.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const Campground = require('../models/campground');
 const cities = require('./cities');
 const {places, descriptors} = require('./seedHelpers');
-mongoose.connect('mongodb://localhost:27017/yelp-camp',{
+mongoose.connect(dbUrl,{
     useNewUrlParser: true, 
     useCreateIndex:true,
     useUnifiedTopology: true
