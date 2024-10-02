b2bfde6e5b3e8854d808afd738938c67a5cdcc33
Chelsey W. <55863583+cwilliams85216@users.noreply.github.com>
2024-10-02 09:34:32 -0500

    Update index.js
    attempt at seeding

--- seeds/index.js
+++ seeds/index.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const Campground = require('../models/campground');
 const cities = require('./cities');
 const {places, descriptors} = require('./seedHelpers');
+const dbUrl =  process.env.DB_URL;
 mongoose.connect(dbUrl,{
     useNewUrlParser: true, 
     useCreateIndex:true,
